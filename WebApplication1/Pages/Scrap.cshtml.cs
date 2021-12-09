cbdd4eaf1e7f0d8c1250bd151be80ee19f4925f8
alybia <alybiacos@gmail.com>
2021-12-09 11:10:53 +0800

    Create README.md

---
+++ WebApplication1/Pages/Scrap.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class ScrapModel : PageModel
+    {
+        private readonly ILogger<ScrapModel> _logger;
+
+        public ScrapModel(ILogger<ScrapModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplication1/Pages/Summary.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class SummaryModel : PageModel
+    { 
+        private readonly ILogger<SummaryModel> _logger;
+
+        public SummaryModel(ILogger<SummaryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
