db0b2bd74379f974a61e2cf86a8d47ee62b3a706
alybia <alybiacos@gmail.com>
2021-12-23 15:58:59 +0800

    Changed UI

--- WebApplication1/Pages/Privacy.cshtml.cs
+++ WebApplication1/Pages/Privacy.cshtml.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
-
+        
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
